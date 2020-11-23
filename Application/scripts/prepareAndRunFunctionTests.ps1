f8ae4d1c781f2e90a49b871790e10804e0491ea1
Anand Upadhyay <anupadh@microsoft.com>
2020-11-23 18:59:59 +0530

    init

---
+++ Application/scripts/prepareAndRunFunctionTests.ps1
@@ -0,0 +1,8 @@
+﻿pushd $env:temp
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path$Installer; Start-Process -FilePath $Path$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path$Installer
+Invoke-WebRequest https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_win32.zip -outFile chromedriver.zip
+Expand-Archive -Path chromedriver.zip
+Remove-Item chromedriver.zip
+popd
+cd testartifacts
+java -DwebAppUrl="https://$\{{ env.APPNAME \}}.azurewebsites.net/" -DChromeWebDriver="$env:temp/chromedriver" -cp SampleWebAppFunctionalTests-jar-with-dependencies.jar org.junit.platform.console.ConsoleLauncher --class-path SampleWebAppFunctionalTests-jar-with-dependencies.jar --scan-class-path --reports-dir reports --include-engine junit-vintage
