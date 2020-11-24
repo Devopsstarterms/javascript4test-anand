bf2982c6b48b9bb5a55d0b2a0b90ec0328317829
Anand Upadhyay <upadhyayap.smit@gmail.com>
2020-11-24 14:40:00 +0530

    Update prepareAndRunFunctionTests.ps1

--- Application/scripts/prepareAndRunFunctionTests.ps1
+++ Application/scripts/prepareAndRunFunctionTests.ps1
@@ -4,5 +4,7 @@ Invoke-WebRequest https://chromedriver.storage.googleapis.com/86.0.4240.22/chrom
 Expand-Archive -Path chromedriver.zip
 Remove-Item chromedriver.zip
 popd
+pwd
+ls
 cd testartifacts
 java -DwebAppUrl="https://$\{{ env.APPNAME \}}.azurewebsites.net/" -DChromeWebDriver="$env:temp/chromedriver" -cp SampleWebAppFunctionalTests-jar-with-dependencies.jar org.junit.platform.console.ConsoleLauncher --class-path SampleWebAppFunctionalTests-jar-with-dependencies.jar --scan-class-path --reports-dir reports --include-engine junit-vintage
